60bfaa18e5031066990c189ed8b058c3f1029ea3
Anna Yue <hummingbird14141@gmail.com>
2024-11-16 14:46:57 -0500

    Update run.sh to handle overflow

--- test/run.sh
+++ test/run.sh
@@ -9,6 +9,15 @@ endEnergy=$(sudo /usr/local/bin/rdmsr 0x611) #intel 0x611, amd 0xc001029b
 endTime_ms=$(date +%s%N | cut -b 1-13)
 totalEnergy=$((0x${endEnergy} - 0x${startEnergy}))
 totalTime_ms=$((endTime_ms - startTime_ms))
+
+if [[$totalEnergy < 0]]; then   # overflow
+        echo "Detected overflow"
+        echo "Start: $startEnergy"
+        echo "End: $endEnergy"
+        startEnergy=$((0x${startEnergy} + 0xffffffff))
+        totalEnergy=$((0x${endEnergy} - 0x${startEnergy}))
+fi
+
 totalTime_seconds=$(bc <<<"scale=2;$totalTime_ms/1000")
 totalEnergy_joules=$(bc <<<"scale=2;$totalEnergy*61/1000000") #15.3 for amd, 61 for intel
 averagePower=$(bc <<<"scale=2;$totalEnergy_joules/$totalTime_seconds")
