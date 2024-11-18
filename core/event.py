d63c94178f06251ae088cee720d09ae46aec2636
Anna Yue <hummingbird14141@gmail.com>
2024-11-18 10:50:45 -0500

    Add class to do bookkeeping for each PID

--- core/event.py
+++ core/event.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from dataclasses import dataclass
+from collections import defaultdict
 
 class EventType(Enum):
     FUNCTION_ENTRY = 0
@@ -21,7 +22,33 @@ class Event:
     core: int
 
 @dataclass
-class Function:
+class Record:
     count: int
     total: int
-    last_time: int
+    dvfs_ran: int
+
+class Process():
+    def __init__(self, pid, core, functions):
+        self.pid = pid
+        self.functions = {f: defaultdict(int) for f in functions}
+        self.core = core
+        self.last_ips = {f: 0 for f in functions}
+
+    def __repr__(self):
+        return f"Process(pid={self.pid}, functions={self.functions}, core={self.core}, last_ips={self.last_ips})"
+
+    def set_time(self, function, ip, time):
+        self.functions[function][ip] = time
+        self.last_ips[function] = ip
+
+    def get_time(self, function, ip):
+        return self.functions[function][ip]
+
+    def reset_last_ip(self, function):
+        self.last_ips[function] = 0
+
+    def get_last_time(self, function):
+        return self.functions[function][self.last_ips[function]]
+
+    def get_core(self):
+        return self.core
