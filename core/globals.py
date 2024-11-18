9fd0be0494aab6ee1bb4dfa06742f3ae3db9924a
Anna Yue <hummingbird14141@gmail.com>
2024-11-18 10:51:30 -0500

    Update globals.py

--- core/globals.py
+++ core/globals.py
@@ -1,6 +1,8 @@
 # Defined Globals
 CPU_INFO = "/proc/cpuinfo"
 FREQUENCY_AMD_LOCATION = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies"
+FREQUENCY_MAX_INTEL_LOCATION = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+FREQUENCY_MIN_INTEL_LOCATION = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
 DEFAULT_DURATION = 75
 DEFAULT_EBPF_PATH = "../ebpf/kernel.c"
 DEFAULT_OUTPUT_FILE = "output.log"
